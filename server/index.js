68ab78389dc974fb258553294e3e970dcd09b06b
Kayla <golderkayla@gmail.com>
2020-07-29 19:24:23 -0400

    add api for getting url preview

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const db = require('../db');
 
@@ -19,17 +20,27 @@ app.get('/tabs', (req, res) => {
 });
 
 app.post('/addtab', (req, res) => {
-  console.log(req.body);
-  const tabData = {
-    website: req.body.website,
-    label: req.body.label,
-  };
-  db.create(tabData, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(result);
-    }
+  axios.post(
+    'https://api.linkpreview.net',
+    {
+      q: req.body.website,
+      key: process.env.TOKEN,
+    },
+  ).then((resp) => {
+    console.log(resp.data);
+    const tabData = {
+      website: req.body.website,
+      label: req.body.label,
+      title: resp.data.title,
+      image: resp.data.image,
+    };
+    db.create(tabData, (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(result);
+      }
+    });
   });
 });
 
